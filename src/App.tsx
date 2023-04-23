98f041fb2a277e6a94718c01ea97c927a37f6ddb
Fernando Cases <nandocasesgarcia@gmail.com>
2023-04-24 00:25:29 +0200

    Feat: fetch data and types doc fully types json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
+import { type User } from './types'
+
 function App() {
+
+  const [users, setUsers] = useState<Array<User[]>>([])
+
+  useEffect(() => {
+    fetch('http://randomuser.me/api?result=100')
+      .then(res => res.json())
+      .then(res => {
+      setUsers(res.results)
+      })
+      .catch(err => 
+      console.error(err))
+  },[])
 
   return (
     <div>
   
       <h1>User List</h1>
+      {JSON.stringify(users)}
     </div>
   )
 }
